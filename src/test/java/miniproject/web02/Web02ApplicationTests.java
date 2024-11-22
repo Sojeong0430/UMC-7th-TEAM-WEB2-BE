9775bab6b37b35f92b0c30e791d3b16549eb79d4
jiyoung <gyjeong021@gmail.com>
2024-11-22 16:57:47 +0900

    🎉 Init : 프로젝트 초기 세팅

---
+++ src/test/java/miniproject/web02/Web02ApplicationTests.java
@@ -0,0 +1,13 @@
+package miniproject.web02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
